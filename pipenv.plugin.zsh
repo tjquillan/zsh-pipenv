761092e0d06d2dd8195015453d99f2dadfc4fce6
iboyperson <tjquillan@gmail.com>
2019-03-12 16:24:07 -0700

    fix issue #2

--- pipenv.plugin.zsh
+++ pipenv.plugin.zsh
@@ -1,4 +1,4 @@
-function pipenv_chpwd() {
+function _pipenv_chpwd() {
     SHOULD_CD=false
 
     # Check if Pipfile exists in pwd or either the parent dir or the parents parent dir
@@ -26,7 +26,10 @@ function pipenv_chpwd() {
 }
 
 # Only add this to the chpwd hooks if pipenv is installed.
+# Additionally if installed run _pipenv_chpwd once to check if the starting
+# directory is a pipenv environment.
 # This means that the shell will likely need to be reloaded if pipenv is installed.
 if command -v pipenv > /dev/null; then
-    chpwd_functions+=(pipenv_chpwd)
+    chpwd_functions+=(_pipenv_chpwd)
+    _pipenv_chpwd
 fi
